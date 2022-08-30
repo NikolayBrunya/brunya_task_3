1894a2756b8481f423f0a2cc4f5e11469a17b521
Nikolay <brunyanp@gmail.com>
2022-08-30 17:51:31 +0700

    Initial commit

---
+++ src/main/java/ru/haulmont/brunya_task_3/BrunyaTask3Application.java
@@ -0,0 +1,13 @@
+package ru.haulmont.brunya_task_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunyaTask3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunyaTask3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/haulmont/brunya_task_3/BrunyaTask3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.haulmont.brunya_task_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunyaTask3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
